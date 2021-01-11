49ded980a2d827e1a9216fb5dfc13c662d22445a
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-11 21:06:16 +0100

    Created a simple entity for getting options settings from config.yml

---
+++ src/cz/MilanT/herocraft/ticketsys/settings/Options.java
@@ -0,0 +1,9 @@
+package cz.MilanT.herocraft.ticketsys.settings;
+
+public class Options {
+    public boolean login_server;
+
+    public Options(boolean login_server) {
+        this.login_server = login_server;
+    }
+}
